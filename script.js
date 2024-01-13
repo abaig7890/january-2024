ae607ab458758b0dc749efd4136cbd0fba5b97f1
abaig7890 <128973169+abaig7890@users.noreply.github.com>
2024-01-13 22:04:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", marks: 80 },
+  { id: 2, name: "jack", age: "20", marks: 85 },
+  { id: 3, name: "karen", age: "19", marks: 35 },
+];
+
+function PrintStudentswithMap() {
+  //PrintDeveloperbyMap() - console.log 
+}
+
+function PrintStudentsbyForEach() {
+PrintDeveloperbyForEach() - console.log
+}
+
+function addData() {
+addData() - In this function make another such employee object and append that in this array
+  push this in the array - {id:4,name:"susan",age:"20",profession:"intern"} and then console.log it. 
+}
+
+function removeFailedStudent() {
+removeAdmin() admin. console.log
+}
+
+function concatenateArray() {
+ {id:4,name:"susan",age:"20",profession:"intern"}
+  {id:4,name:"arohi",age:"25",profession:"intern"} 
+  {id:4,name:"vijay",age:"26",profession:"intern"} 
+}
